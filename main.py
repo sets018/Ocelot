c790f6aae694ef04a2e644d2b4313b500b217774
sets018 <91557108+sets018@users.noreply.github.com>
2022-05-31 14:53:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -726,20 +726,20 @@ input_columns_num = ['Area','bedrooms','bathrooms','garages']
 
 input_columns_cat = ['Borough','neighborhood','condition','estrato','property_type']
 input_columns_num = ['Area','bedrooms','bathrooms','garages']
-
-for column in input_columns_cat:
-    if (column == 'Borough'):
-        borough_input = user_input(column, 'radio', sectors.sectors_values, 'list', cat_input)
-        sectors.get_hoods(borough_input.user_input)
-    elif (column == 'neighborhood'):
-        hoods_input = user_input(column, 'radio', sectors.hood_list, 'list', cat_input)
-    else:
-        usr_input_cat = user_input(column, 'radio', df , 'dataframe', cat_input)
-        #cat_input.append(usr_input_cat.user_input)
+with st.siderbar:
+    for column in input_columns_cat:
+        if (column == 'Borough'):
+            borough_input = user_input(column, 'radio', sectors.sectors_values, 'list', cat_input)
+            sectors.get_hoods(borough_input.user_input)
+        elif (column == 'neighborhood'):
+            hoods_input = user_input(column, 'radio', sectors.hood_list, 'list', cat_input)
+        else:
+            usr_input_cat = user_input(column, 'radio', df , 'dataframe', cat_input)
+            #cat_input.append(usr_input_cat.user_input)
+    for column in input_columns_num:
+        usr_input_num = user_input(column, 'slider', df, 'dataframe', num_input)
+        #num_input.append(usr_input_num.user_input)
     
-for column in input_columns_num:
-    usr_input_num = user_input(column, 'slider', df, 'dataframe', num_input)
-    #num_input.append(usr_input_num.user_input)
 price_plots = plotting(df)
 price_plots.dist('Price')
 price_plots.scatter('Area')
